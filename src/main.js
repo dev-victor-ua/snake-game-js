214f006003b665462b96e0cade8ad1aebaa2765f
Viktor Lytvynchuk <dev.victor.lytv@gmail.com>
2023-02-14 21:16:15 +0200

    Code organisation: - Moved inline css to scss file. - Added dynamic js/css injection to HTML.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './scss/main.scss';
 import { initGame } from './snake';
 
 initGame();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const webpack = require('webpack');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const config = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: './js/bundle.js'
   },
   devServer: {
     static: {
@@ -18,14 +20,36 @@ const config = {
     compress: true,
     port: 5342
   },
-  plugins: [new ESLintPlugin({fix: true})],
+  plugins: [
+    new ESLintPlugin({fix: true}),
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].css",
+      chunkFilename: "./css/[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Game',
+      // Load a custom template (lodash by default)
+      template: './public/index.html'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.s[ac]ss$/i, // test for .sass or .scss extension
+        use: [
+          // Load style-loader or mini-css-extract-plugin-loader
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ]
   }
 };
